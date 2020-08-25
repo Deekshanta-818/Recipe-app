06be99386a2f009f3311a5e7a0f9a6c9234a0055
Deekshanta-818 <deekshantapaudel@gmail.com>
2020-08-25 18:12:14 +0545

    My commit

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} placeholder='Search for a food'/>
         <button className="search-button" type="submit">
           Search
         </button>
